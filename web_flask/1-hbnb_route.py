a8d07a90b6bc1cf23f282fbec733c816db22de3c
Boozmanras <paulvictoraa@gmail.com>
2024-08-23 21:12:54 +0300

    changd

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """A simple Flask web application with multiple routes."""
-from web_flask import app
+from flask import Flask
+
+app = Flask(__name__) 
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
